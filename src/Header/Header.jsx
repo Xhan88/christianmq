f5e2faa396cd3b49db89b4f7bc1914973e0a0196
Christian Martinez <dantedeat@hotmail.com>
2025-02-25 10:59:46 -0600

    header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,9 +2,18 @@ import "./header.css"
 
 const Header = () => {
   return (
-    <div>
-      <h1>hello world</h1>
+    <>
+    <div className="gretting-container">
+      <p className="greting">
+          Hi, My name is
+      </p>
+      <h1 className="name">Christian Martínez.</h1>
+      <h2 className="name-tex-section">I build things for the web.</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor in sunt aspernatur suscipit consectetur, dignissimos commodi. Labore dolore est quibusdam repellat repellendus! Praesentium minus odio debitis aut distinctio expedita repellendus.</p>
     </div>
+       
+    </>
+   
   )
 }
 
