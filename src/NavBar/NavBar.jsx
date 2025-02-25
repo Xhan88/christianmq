399fe4c7ce8e3d659a1e7531718bab866a4167cc
Christian Martinez <dantedeat@hotmail.com>
2025-02-24 19:25:23 -0600

    allnavbarcomplete

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -4,13 +4,16 @@ const NavBar = () => {
   return (
     <>
     <div className="nav-container">
-      <img className="img-nav" src="./backgroundmq.jpg" alt="" />
-      <ol>
+      <div className="img-container">
+         <img className="img-nav" src="./481.png" alt="" />
+      </div>
+     
+      <ol className="list">
           <li><span>01.</span>About</li>
           <li><span>02.</span>Experience</li>
           <li><span>03.</span>Work</li>
           <li><span>04.</span>Contact</li>
-          <button>
+          <button className="btn-navbar">
               Resume
           </button>
       </ol>
