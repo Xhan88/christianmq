c69255b0380bbdc83fbe3d2a8fcf30f9cd0b2114
Christian Martinez <dantedeat@hotmail.com>
2025-02-25 13:43:37 -0600

    25-2-25

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -10,7 +10,15 @@ const Header = () => {
       <h1 className="name">Christian Martínez.</h1>
       <h2 className="name-tex-section">I build things for the web.</h2>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor in sunt aspernatur suscipit consectetur, dignissimos commodi. Labore dolore est quibusdam repellat repellendus! Praesentium minus odio debitis aut distinctio expedita repellendus.</p>
-    </div>
+      <a 
+          href="https://github.com/Xhan88" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="btn-github"
+        >
+          Check out my GitHub!
+        </a>
+     </div>
        
     </>
    
