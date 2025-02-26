4b08523b80808946c95091ceb21bc2344bfe010f
Christian Martinez <dantedeat@hotmail.com>
2025-02-26 09:37:11 -0600

    26/02/25

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       </p>
       <h1 className="name">Christian Martínez.</h1>
       <h2 className="name-tex-section">I build things for the web.</h2>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor in sunt aspernatur suscipit consectetur, dignissimos commodi. Labore dolore est quibusdam repellat repellendus! Praesentium minus odio debitis aut distinctio expedita repellendus.</p>
+      <p className="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor in sunt aspernatur suscipit consectetur, dignissimos commodi. Labore dolore est quibusdam repellat repellendus! Praesentium minus odio debitis aut distinctio expedita repellendus.</p>
       <a 
           href="https://github.com/Xhan88" 
           target="_blank" 
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react"
 import "./navbar.css"
 
 const NavBar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <>
     <div className="nav-container">
       <div className="img-container">
          <img className="img-nav" src="./481.png" alt="" />
       </div>
+
+      <div className={`hamburguesa ${menuOpen ? "open": ""}`} onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
      
-      <ol className="list">
+      <ol className={`list ${menuOpen ? "open" : ""}`}>
           <li><span>01.</span>About</li>
           <li><span>02.</span>Experience</li>
           <li><span>03.</span>Work</li>
