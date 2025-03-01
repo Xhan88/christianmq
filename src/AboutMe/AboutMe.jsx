b98134ddb7f4b4c205537660f5ab5759efb8631c
Christian Martinez <dantedeat@hotmail.com>
2025-03-01 06:44:45 -0600

    picchange

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -6,13 +6,13 @@ const AboutMe = () => {
      <div className="text-container">
       <h1><span>01.</span> About Me  ──────────────</h1>
       <p>
-      Hello! I'm a front-end developer passionate about crafting responsive and dynamic web experiences. My journey into web development started four years ago, and since then, I've been honing my skills in JavaScript, React.js, HTML, CSS, and WordPress.
+      Hello! I’m a front-end developer passionate about crafting responsive and dynamic web experiences. My journey into web development started four years ago, and since then, I’ve been honing my skills in JavaScript, React.js, HTML, CSS, and WordPress.
       </p>
       <p>
       I have experience working with Elementor, Bootstrap, MUI, and GitHub, building and optimizing websites for performance and user experience. I enjoy bringing designs to life and ensuring seamless interactions for users across different devices.
       </p>
       <p>
-      Currently, I'm focused on improving my problem-solving skills in JavaScript, deepening my understanding of React.js, and staying updated with modern web development trends. I'm always eager to take on new challenges and grow as a developer.
+      Currently, I’m focused on improving my problem-solving skills in JavaScript, deepening my understanding of React.js, and staying updated with modern web development trends. I’m always eager to take on new challenges and grow as a developer.
       </p>
       <p>Technologies I’ve been working with recently:</p>
       <ul className='order-list'>
@@ -24,7 +24,7 @@ const AboutMe = () => {
         <li><span>▻</span>   WordPress</li>
       </ul>
       </div>
-    <div >
+    <div className="img-container-chan">
         <img className='img-chan'src="./482.jpg" alt="" />
     </div>
       
