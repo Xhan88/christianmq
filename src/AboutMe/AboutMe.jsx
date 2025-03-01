0e330979bfb05ec8adfa48a9fb27f656a55a0fc2
Christian Martinez <dantedeat@hotmail.com>
2025-02-28 23:26:17 -0600

    28/02/205ß

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -4,15 +4,15 @@ const AboutMe = () => {
   return (
     <div className="about-me-container">
      <div className="text-container">
-      <h1><span>01.</span> About Me  ───────────────</h1>
+      <h1><span>01.</span> About Me  ──────────────</h1>
       <p>
-        Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
+      Hello! I'm a front-end developer passionate about crafting responsive and dynamic web experiences. My journey into web development started four years ago, and since then, I've been honing my skills in JavaScript, React.js, HTML, CSS, and WordPress.
       </p>
       <p>
-        Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
+      I have experience working with Elementor, Bootstrap, MUI, and GitHub, building and optimizing websites for performance and user experience. I enjoy bringing designs to life and ensuring seamless interactions for users across different devices.
       </p>
       <p>
-        I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
+      Currently, I'm focused on improving my problem-solving skills in JavaScript, deepening my understanding of React.js, and staying updated with modern web development trends. I'm always eager to take on new challenges and grow as a developer.
       </p>
       <p>Technologies I’ve been working with recently:</p>
       <ul className='order-list'>
