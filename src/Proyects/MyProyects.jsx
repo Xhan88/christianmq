46185a9d618f95522a6615cf8a1438ceeac77e63
Christian Martinez <dantedeat@hotmail.com>
2025-03-05 08:35:22 -0600

    5-marzo-2025

--- src/Proyects/MyProyects.jsx
+++ src/Proyects/MyProyects.jsx
@@ -3,25 +3,72 @@ import './MyProyects.css'
 const MyProyects = () => {
   return (
     <div>
-      <h1 className="section-three"><span>03. </span>Some Things I’ve Built ──────────────</h1>
-    
-        
+      <h1 className="section-three"><span>03. </span>Some Things I’ve Built ────────────</h1> 
         <section className='style-works'>
-        <div>
+        <div className='img-container2'>
             <img className='img-proyects' src="./483.png" alt="works" />
-            </div>
-            
+        </div>       
             <div className='information-works'>
                         <p className='studie-proyects'>Lorem ipsum dolor </p>
-                    <h3>hello world</h3>
-                    <p className='content-description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi beatae autem nemo asperiores veritatis quia dignissimos inventore nihil aliquid nostrum. ignissimos inventore nihil aliquid nostrumignissimos inventore nihil aliquid nostrum</p>
+                        <h3>hello world</h3>
+                        <p className='content-description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi beatae autem nemo asperiores veritatis quia dignissimos inventore nihil aliquid nostrum. ignissimos inventore nihil aliquid nostrumignissimos inventore nihil aliquid nostrum</p>
                     <ul className='list-tecnology'>
                             <li>lorem</li>
                             <li>lorem</li>
                             <li>lorem</li>
-                            <li>lorem</li>
-                            
+                            <li>lorem</li>                           
                     </ul>
+
+                    <div>
+                        <a href="">github</a>
+                        <a href="">page</a>
+                    </div>
+            </div>
+            
+            
+           
+        </section>
+
+        <section className='style-works'>
+        <div>
+            <img className='img-proyects' src="./483.png" alt="works" />
+            </div>       
+            <div className='information-works'>
+                        <p className='studie-proyects'>Lorem ipsum dolor </p>
+                        <h3>hello world</h3>
+                        <p className='content-description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi beatae autem nemo asperiores veritatis quia dignissimos inventore nihil aliquid nostrum. ignissimos inventore nihil aliquid nostrumignissimos inventore nihil aliquid nostrum</p>
+                    <ul className='list-tecnology'>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>                           
+                    </ul>
+
+                    <div>
+                        <a href="">github</a>
+                        <a href="">page</a>
+                    </div>
+            </div>
+            
+            
+           
+        </section>
+
+        <section className='style-works'>
+        <div>
+            <img className='img-proyects' src="./483.png" alt="works" />
+            </div>       
+            <div className='information-works'>
+                        <p className='studie-proyects'>Lorem ipsum dolor </p>
+                        <h3>hello world</h3>
+                        <p className='content-description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi beatae autem nemo asperiores veritatis quia dignissimos inventore nihil aliquid nostrum. ignissimos inventore nihil aliquid nostrumignissimos inventore nihil aliquid nostrum</p>
+                    <ul className='list-tecnology'>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>
+                            <li>lorem</li>                           
+                    </ul>
+
                     <div>
                         <a href="">github</a>
                         <a href="">page</a>
