425d698557793c275e91ad62e562927b9c63ae4e
Christian Martinez <dantedeat@hotmail.com>
2025-03-14 21:13:35 -0600

    cervezaconyakulth

--- src/Proyects/MyProyects.jsx
+++ src/Proyects/MyProyects.jsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './MyProyects.css'
 
 const MyProyects = () => {
@@ -14,7 +15,7 @@ const MyProyects = () => {
             <div className='information-works'>
                         <p className='studie-proyects'>Christian Martinez </p>
                         <h3>Profecional Butcher</h3>
-                        <p className='content-description'>My professional butcher presentation website, developed with React,<span> HTML, and CSS</span> , offers an engaging visual experience with Animate.css animations and <span> AOS</span>  hover effects. It showcases my expertise in boning, fine cuts, and customer service, highlighting my background in the meat industry with a modern and dynamic design.</p>
+                        <p className='content-description'>My professional butcher presentation website, developed with React,<span> HTML, and CSS</span> , offers an engaging visual experience with<span> Animate.css</span>  animations and <span> AOS</span>  hover effects. It showcases my expertise in boning, fine cuts, and customer service, highlighting my background in the meat industry with a modern and dynamic design.</p>
                     <ul className='list-tecnology'>
                             <li>HTML</li>
                             <li>CSS</li>
@@ -23,7 +24,7 @@ const MyProyects = () => {
                     </ul>
 
                     <div>
-                        <a href="">github</a>
+                    <FontAwesomeIcon icon="fa-brands fa-github" />
                         <a href="">page</a>
                     </div>
             </div>
@@ -39,10 +40,10 @@ const MyProyects = () => {
                         <h3>Susy Sanchez</h3>
                         <p className='content-description-reverce'>I developed a website for a clinical pedicure salon using React, HTML, CSS, React Router DOM, and MUI. This project, created after graduating from the Coderhouse bootcamp, helped improve the business`s digital presence, increasing its client base by 20% and providing a modern and intuitive experience.</p>
                     <ul className='list-tecnology'>
-                            <li>lorem</li>
-                            <li>lorem</li>
-                            <li>lorem</li>
-                            <li>lorem</li>                           
+                            <li>React.js</li>
+                            <li>ReactRouterDom</li>
+                            <li>MUI</li>
+                            <li>CSS</li>                           
                     </ul>
 
                     <div>
