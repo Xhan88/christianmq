b6459f36ef186e99486d06fb16692cd5b76df9f9
Christian Martinez <dantedeat@hotmail.com>
2025-03-24 07:00:43 -0600

    24-march-2025

--- src/Trabajos/WorkExperience.jsx
+++ src/Trabajos/WorkExperience.jsx
@@ -3,13 +3,13 @@ import { useState, forwardRef } from "react";
 import "./WorkExperience.css"; 
 
 const experiences = [
-  { id: 1, company: "Carniceria Matamoros- (Wordpress) ", description: "Website Development for Carnicería Matamoros As part of my web development experience, I had the opportunity to design and create a website for Carnicería Matamoros, using WordPress and the Elementor visual builder. The goal of the project was to provide a professional and functional online presence to improve the business's visibility and facilitate customer interaction." },
+  { id: 1, company: "Carniceria Matamoros- (Wordpress) ", url: "https://cristian-pruebas-wordpress-1.617iip.easypanel.host/", description: "Website Development for Carnicería Matamoros As part of my web development experience, I had the opportunity to design and create a website for Carnicería Matamoros, using WordPress and the Elementor visual builder. The goal of the project was to provide a professional and functional online presence to improve the business's visibility and facilitate customer interaction." },
 
-  { id: 2, company: "Taqueria del Abuelo- (Wordpress)", description: "Website Development for Taquería del Abuelo I developed a website for Taquería del Abuelo using WordPress and Elementor, with a responsive design that adapts to mobile devices, tablets, and computers. The site includes a product menu with the taqueria's menu options, an image gallery to showcase the dishes, and a contact form to facilitate customer interaction. I also integrated an interactive map to easily locate the taqueria. I added animations to enhance the visual experience and make navigation more dynamic." },
+  { id: 2, company: "Taqueria del Abuelo- (Wordpress)",url: "https://cristian-pruebas-wordpress-2.617iip.easypanel.host/", description: "Website Development for Taquería del Abuelo I developed a website for Taquería del Abuelo using WordPress and Elementor, with a responsive design that adapts to mobile devices, tablets, and computers. The site includes a product menu with the taqueria's menu options, an image gallery to showcase the dishes, and a contact form to facilitate customer interaction. I also integrated an interactive map to easily locate the taqueria. I added animations to enhance the visual experience and make navigation more dynamic." },
 
-  { id: 3, company: "Hero Finder - Aplicación de Héroes - (React.js)", description: "Hero Finder is a web application developed in React.js that allows you to explore heroes from DC Comics and Marvel. It implements the React Router DOM to handle navigation between sections, a dynamic search engine that filters heroes in real time, and a login authentication system. Animate.css is also incorporated for elegant transitions, improving the user experience."},
+  { id: 3, company: "Hero Finder - Aplicación de Héroes - (React.js)",url: "https://monumental-madeleine-05800e.netlify.app/marvel", description: "Hero Finder is a web application developed in React.js that allows you to explore heroes from DC Comics and Marvel. It implements the React Router DOM to handle navigation between sections, a dynamic search engine that filters heroes in real time, and a login authentication system. Animate.css is also incorporated for elegant transitions, improving the user experience."},
 
-  { id: 4, company: "My first Portafolio- (react.js)", description: "This is a dynamic and modern web portfolio developed with React.js, designed to showcase my skills, projects, and experience as a Front-End Developer. It uses React Router DOM for fluid navigation, Bootstrap for a stylish and responsive structure, and Animate.css for elegant animations.    " },
+  { id: 4, company: "My first Portafolio- (react.js)",url: "https://react-web-site-martinez.netlify.app/", description: "This is a dynamic and modern web portfolio developed with React.js, designed to showcase my skills, projects, and experience as a Front-End Developer. It uses React Router DOM for fluid navigation, Bootstrap for a stylish and responsive structure, and Animate.css for elegant animations.    " },
   
 ];
 
@@ -31,6 +31,7 @@ const WorkExperience = forwardRef((props, ref) => {
             style={{ transform: `translateY(${(selectedId - 1) * itemHeight}px)` }}
           ></div>
           {experiences.map((exp) => (
+            
             <button
               key={exp.id}
               className={`company-button ${selectedId === exp.id ? "active" : ""}`}
@@ -38,10 +39,16 @@ const WorkExperience = forwardRef((props, ref) => {
             >
               {exp.company}
             </button>
+
           ))}
         </div>
         <div className="description2">
-          <h2 className="work-title">{experiences.find((exp) => exp.id === selectedId).company}</h2>
+          <a className="title-link" href={experiences.find((exp) => exp.id === selectedId).url} 
+            target="_blank" 
+            rel="noopener noreferrer">
+            <h2 className="work-title">{experiences.find((exp) => exp.id === selectedId).company}</h2>
+          </a>
+          
           <p>
             <span>  ▻ </span>{experiences.find((exp) => exp.id === selectedId).description}</p>
         </div>
