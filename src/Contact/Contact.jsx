0e412dab3252893161c126edceebeaadd9494ef1
Christian Martinez <dantedeat@hotmail.com>
2025-03-25 07:09:14 -0600

    25-march-2025

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -9,6 +9,7 @@ const Contact = React.forwardRef((props, ref) => {
            <h1 className="end-title"> 04. What’s Next?</h1>
             <h2>Get In Touch</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore reiciendis itaque placeat, illum veritatis praesentium a architecto eius dolorum voluptas minima aliquid, odio consequuntur optio at iste modi atque. Iusto!</p>
+            
             <button className="btn-email">Say Hello</button>
           </div>
           
